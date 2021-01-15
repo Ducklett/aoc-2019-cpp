0eed2ceadadd26e5911d258b27d4cac2fe08f209
Ducklett <ivy16203@gmail.com>
2021-01-15 21:03:33 +0100

    create cli day picker

--- aocpp/main.cpp
+++ aocpp/main.cpp
@@ -8,5 +8,35 @@
 
 int main()
 {
-	day6::run();
+	using namespace std;
+
+	bool immediate = true;
+
+	if (immediate) {
+		day6::run();
+	}
+	else {
+		int chosenDay = -1;
+		string chosen;
+		while (true) {
+			cout << "Choose a day to run (1-25): ";
+			cin >> chosenDay;
+			cout << chosenDay << endl;
+
+			if (chosenDay < 1 || chosenDay>25) break;
+
+			switch (chosenDay)
+			{
+			case 1: day1::run(); break;
+			case 2: day2::run(); break;
+			case 3: day3::run(); break;
+			case 4: day4::run(); break;
+			case 5: day5::run(); break;
+			case 6: day6::run(); break;
+			default:
+				cout << "not implemented yet" << endl;
+				exit(1);
+			}
+		}
+	}
 }
